6c54dc5ee32464fdc50e87a760d8435783ec17e3
cathyleeu <leeyu823@gmail.com>
2017-04-20 02:47:06 +0900

    api for comment

--- src/api/feed/feed.model.js
+++ src/api/feed/feed.model.js
@@ -1,25 +1,27 @@
 import mongoose from 'mongoose'
 // import autoIncrement from'mongoose-auto-increment'
 
+
+const Comment = new mongoose.Schema({
+  name: String,
+  univ: String,
+  userImg: String,
+  comment: { type: String, required: true },
+  createdOn: { type: Date, default: Date.now },
+})
+
 const Feed = new mongoose.Schema({
   typeOf: String, //전체공개 or 대나무숲
   name: String,
   univ: String,
-  userImg: { type: String, default: 'Avatar' },
+  userImg: String,
   createdOn: { type: Date, default: Date.now },
   content: { type: String, required: true },
   postImg: String,
   // location: { type: String, default: null },
   likes:[{type: mongoose.Schema.Types.ObjectId, ref: 'user'}],
   // imageSource: { type: String, default: 'null' },
-  comment:[{
-    c_no: Number,
-    c_username: String,
-    c_useruniv: String,
-    c_userimg: { type: String, default: 'Avatar' },
-    c_usercmt: { type: String, required: true },
-    c_posted: { type: Date, default: Date.now },
-  }],
+  comment:[Comment],
 }, {collection: 'feed'})
 
 export default mongoose.model('feed', Feed)
